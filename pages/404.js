b29a85f4012531c582de4f1c8894fca387244d56
s3539822 <s3539822@student.rmit.edu.au>
2020-12-25 22:48:35 +1100

    Refactor

--- pages/404.js
+++ pages/404.js
@@ -1,48 +1,14 @@
 import React from "react";
+import Router from "next/router";
 
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-
-import m404Background from "assets/img/bg7.jpg";
-import errorPageStyle from "assets/jss/nextjs-material-kit-pro/pages/errorPageStyles.js";
-import HeaderFooterLayout from "../layouts/HeaderFooterLayout";
-
-const useStyles = makeStyles(errorPageStyle);
+import URLS from "assets/strings/urls";
 
 export default function ErrorPage({ ...rest }) {
     React.useEffect(() => {
-        window.scrollTo(0, 0);
-        document.body.scrollTop = 0;
+        Router.push(URLS.LANDING_PAGE, URLS.ROOT);
     });
-    const classes = useStyles();
-    return (
-        <HeaderFooterLayout>
 
-            <div
-                className={classes.pageHeader}
-                style={{
-                    backgroundImage: "url(" + m404Background + ")",
-                    backgroundSize: "cover",
-                    backgroundPosition: "top center"
-                }}
-            >
-                <div className={classes.contentCenter}>
-                    <Grid
-                        container
-                        direction={"row"}
-                        alignContent={"center"}
-                        justify={"center"}
-                    >
-                        <Grid item md={12}>
-                            <h1 className={classes.title}>404</h1>
-                            <h2 className={classes.subTitle}>Page not found :(</h2>
-                            <h4 className={classes.description}>
-                                Oooops! Looks like you got lost.
-                            </h4>
-                        </Grid>
-                    </Grid>
-                </div>
-            </div>
-        </HeaderFooterLayout>
+    return (
+        <div />
     );
 }
--- pages/_error.js
+++ pages/_error.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import Router from "next/router";
 
+import URLS from "assets/strings/urls";
+
 export default class _error extends Component {
   componentDidMount = () => {
-    Router.push("/demo/presentation");
+    Router.push(URLS.LANDING_PAGE, URLS.ROOT);
   };
 
   render() {
