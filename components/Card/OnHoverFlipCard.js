d490beceaccb4cbc9ff1e1b2de6e09270476527a
s3539822 <s3539822@student.rmit.edu.au>
2020-12-25 17:50:34 +1100

    Animation

--- components/Card/OnHoverFlipCard.js
+++ components/Card/OnHoverFlipCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from "prop-types"
 import ReactCardFlip from "react-card-flip";
 import useHover from "@react-hook/hover";
+import { useInView } from 'react-intersection-observer';
 
 import {Grid} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles"
@@ -26,34 +27,43 @@ export default function OnHoverFlipCard(props) {
     const {frontImage, backText, flipDirection} = props
     const classes = useStyles()
 
-    const target = React.useRef(null)
-    const isHovering = useHover(target, {enterDelay: 50, leaveDelay: 100})
+    const [ref, inView] = useInView({
+        triggerOnce: false,
+        threshold: .8,
+    });
+
+    const target = React.useRef([])
+    const isHovering = useHover(target, {enterDelay: 250, leaveDelay: 100})
+
+    const flipped = isHovering ? false : inView
 
     return (
-        <div style={{margin: "10px"}} ref={target}>
-            <ReactCardFlip  isFlipped={isHovering} flipDirection={flipDirection}>
-                <div className={classes.card}>
-                    <img src={frontImage} style={{width: "80%", height: "80%", margin: "10% 10%"}}/>
-                </div>
+        <div ref={target}>
+            <div style={{margin: "10px"}} ref={ref}>
+                <ReactCardFlip  isFlipped={flipped} flipDirection={flipDirection}>
+                    <div className={classes.card}>
+                        <img src={frontImage} style={{width: "80%", height: "80%", margin: "10% 10%"}}/>
+                    </div>
 
-                <div className={classes.card}>
-                    <Grid
-                        container
-                        direction={"column"}
-                        justify={"center"}
-                        alignItems={"center"}
-                        className={classes.container}
-                    >
-                        <Grid item>
-                            <h2 style={{color: "black"}}>
-                                <b>
-                                    {backText}
-                                </b>
-                            </h2>
+                    <div className={classes.card}>
+                        <Grid
+                            container
+                            direction={"column"}
+                            justify={"center"}
+                            alignItems={"center"}
+                            className={classes.container}
+                        >
+                            <Grid item>
+                                <h2 style={{color: "black"}}>
+                                    <b>
+                                        {backText}
+                                    </b>
+                                </h2>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </div>
-            </ReactCardFlip>
+                    </div>
+                </ReactCardFlip>
+            </div>
         </div>
     )
 }
