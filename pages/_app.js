dc216231f295c40ecd25b960a8fea7e7efabb2a2
s3539822 <41284457+s3539822@users.noreply.github.com>
2020-12-26 23:08:24 +1100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -34,16 +34,6 @@ Router.events.on("routeChangeError", () => {
 config({ ssrFadeout: true });
 
 export default class MyApp extends App {
-    /*static async getInitialProps({ Component, router, ctx }) {
-        let pageProps = {};
-
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
-        }
-
-        return { pageProps };
-    }*/
-
     render() {
         const { Component, pageProps } = this.props;
 
