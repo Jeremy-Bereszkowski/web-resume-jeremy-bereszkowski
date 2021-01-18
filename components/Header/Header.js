b151c08af5472e88cdccb465318bc935f42d3317
s3539822 <41284457+s3539822@users.noreply.github.com>
2021-01-18 20:34:58 +1100

    Update Header.js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -6,7 +6,7 @@
 import React from "react";
 import classNames from "classnames";
 
-import {Grid, Hidden, Icon} from "@material-ui/core";
+import {Grid} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 
