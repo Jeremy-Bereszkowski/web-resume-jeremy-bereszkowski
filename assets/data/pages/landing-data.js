62155be6d5e124665f1242822f4b4b12b5dcc60b
s3539822 <41284457+s3539822@users.noreply.github.com>
2021-01-18 22:01:25 +1100

    Update landing-data.js

--- assets/data/pages/landing-data.js
+++ assets/data/pages/landing-data.js
@@ -32,15 +32,9 @@ const LandingData = {
             subHeader: "A computer science and engineering " +
                 "student at RMIT University from Melbourne, Australia.",
             para: [
-                "Highly organized and detail-oriented honors student with " +
-                "a strong focus on consumer facing design and experiences.",
-                "Having experience across a wide range of technologies " +
-                "and frameworks including ReactJS, Android and cloud infrastructures " +
-                "including AWS & Google Cloud.",
-                "Studying a double degree in both software and hardware, this has " +
-                "provided me with a unique perspective and approach when problem " +
-                "solving projects. I look forward to bringing this distinctive viewpoint" +
-                " into all aspects of my work."
+                "Highly organized and detail-oriented honours student with a strong focus on consumer-facing design and experiences.",
+                "Having experience across a wide range of technologies and frameworks including ReactJS, Android and cloud infrastructures including AWS & Google Cloud.",
+                "Studying a double degree in both software and hardware, this has provided me with a unique perspective and approach when problem-solving projects. I look forward to bringing this distinctive viewpoint into all aspects of my work.",
             ]
         },
         avatar: Avatar,
