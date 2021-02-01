2249252cf4a0a5a9e766c875abd334c359af57f2
s3539822 <41284457+s3539822@users.noreply.github.com>
2021-02-01 20:45:57 +1100

    Update Projects.js

--- page-sections/landing-page/Projects.js
+++ page-sections/landing-page/Projects.js
@@ -20,9 +20,6 @@ const useStyles = makeStyles(theme => ({
         color: Colours.ternary,
         fontWeight: "600!important",
         fontSize: "1rem!important",
-    },
-    border: {
-        padding: "0 20px",
     }
 }))
 
@@ -41,7 +38,6 @@ export default function Projects(props) {
             <br/>
             <br/>
             <br/>
-            <div className={classes.border}>
             <VerticalTimeline animate={!animate}>
                 {
                     cards.map((ele, key) => {
@@ -70,68 +66,48 @@ export default function Projects(props) {
                                     {leftButton}
                                     {rightButton}
                                 </Grid>
-                            ) : null
-                            const rightButton = ele.href2 !== undefined ? (
-                                <Grid item>
-                                    <BlockButton color={"green"} href={ele.href2}>
-                                        {ele.text2}
-                                    </BlockButton>
-                                </Grid>
-                            ) : null
-                            const buttonGroup = leftButton || rightButton !== null ? (
-                                <Grid item>
-                                    <Grid
-                                        container
-                                        direction={"row"}
-                                        justify={"flex-end"}
-                                        alignItems={"center"}
-                                    >
-                                        {leftButton}
-                                        {rightButton}
-                                    </Grid>
-                                </Grid>
-                            ) : null
-                            return (
-                                <VerticalTimelineElement
-                                    className="vertical-timeline-element--work"
-                                    date="2020"
-                                    dateClassName={classes.date}
-                                    iconStyle={{ background: ele.color, color: '#fff' }}
-                                    icon={<StarIcon />}
-                                    key={key}
+                            </Grid>
+                        ) : null
+                        return (
+                            <VerticalTimelineElement
+                                className="vertical-timeline-element--work"
+                                date="2020"
+                                dateClassName={classes.date}
+                                iconStyle={{ background: ele.color, color: '#fff' }}
+                                icon={<StarIcon />}
+                                key={key}
+                            >
+                                <Grid
+                                    container
+                                    direction={"column"}
+                                    justify={"center"}
+                                    alignItems={"flex-start"}
                                 >
-                                    <Grid
-                                        container
-                                        direction={"column"}
-                                        justify={"center"}
-                                        alignItems={"flex-start"}
-                                    >
-                                        <Grid item>
-                                            <h3 className="vertical-timeline-element-title">
-                                                {ele.title}
-                                            </h3>
-                                        </Grid>
-                                        <Grid item>
-                                            <h4 className="vertical-timeline-element-subtitle">
-                                                {ele.subTitle}
-                                            </h4>
-                                        </Grid>
-                                        <Grid item>
-                                            <p>
-                                                {ele.para}
-                                            </p>
-                                        </Grid>
-                                        <Grid item>
-                                            <br/>
-                                        </Grid>
-                                        {buttonGroup}
+                                    <Grid item>
+                                        <h3 className="vertical-timeline-element-title">
+                                            {ele.title}
+                                        </h3>
                                     </Grid>
-                                </VerticalTimelineElement>
-                            )
-                        })
-                    }
-                </VerticalTimeline>
-            </div>
+                                    <Grid item>
+                                        <h4 className="vertical-timeline-element-subtitle">
+                                            {ele.subTitle}
+                                        </h4>
+                                    </Grid>
+                                    <Grid item>
+                                        <p>
+                                            {ele.para}
+                                        </p>
+                                    </Grid>
+                                    <Grid item>
+                                        <br/>
+                                    </Grid>
+                                    {buttonGroup}
+                                </Grid>
+                            </VerticalTimelineElement>
+                        )
+                    })
+                }
+            </VerticalTimeline>
         </HeaderSubHeaderBody>
     )
 }
