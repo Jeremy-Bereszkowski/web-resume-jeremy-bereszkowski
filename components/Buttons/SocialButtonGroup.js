81d32b0656c24516c552ec13dc489dba61c16727
s3539822 <41284457+s3539822@users.noreply.github.com>
2021-02-07 16:10:07 +1100

    Update SocialButtonGroup.js

--- components/Buttons/SocialButtonGroup.js
+++ components/Buttons/SocialButtonGroup.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import classNames from "classnames";
 
+import {makeStyles} from "@material-ui/core/styles";
 import {Grid} from "@material-ui/core";
 
 import FooterButton from "./FooterButton";
 
+import {defaultFont} from "../../assets/jss/nextjs-material-kit-pro";
+import URLS from "../../assets/strings/urls";
+import Colours from "../../assets/strings/colours";
+
+const useStyles = makeStyles({
+    title: {
+        letterSpacing: "unset",
+        "&,& a": {
+            ...defaultFont,
+            minWidth: "unset",
+            lineHeight: "30px",
+            fontSize: "18px",
+            borderRadius: "3px",
+            textTransform: "none",
+            whiteSpace: "nowrap",
+            color: "inherit",
+        },
+        color: "white",
+        marginTop: "14px!important",
+        marginBottom: "7px!important",
+    },
+    link: {
+        "&:hover, &:focus": {
+            color: Colours.primary
+        }
+    },
+    margin: {
+        marginLeft: "4px",
+    }
+});
+
 export default function SocialButtonGroup(props) {
+    const classes = useStyles();
+
     return (
         <Grid
             container
@@ -14,24 +49,24 @@ export default function SocialButtonGroup(props) {
             alignItems={"center"}
         >
             <Grid item>
-                {/*<FooterButton
-                    href="https://www.linkedin.com/in/jeremy-bereszkowski-999ba016a/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <i className="fab fa-linkedin-in"/>
-                </FooterButton>*/}
+                <Link>
                 <FooterButton
                     href="https://www.linkedin.com/in/jeremy-bereszkowski-999ba016a/"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <i>
-                        <h4>
+                    <h2 className={classes.title}>
+                        <a
+                            className={classNames(classes.link, classes.margin)}
+                            href={URLS.PAPER_RESUME}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
                             CV
-                        </h4>
-                    </i>
+                        </a>
+                    </h2>
                 </FooterButton>
+                </Link>
             </Grid>
             <Grid item>
                 <FooterButton
